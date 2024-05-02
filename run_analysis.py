1b595e121e3e667e4bd7619a560519e76a1dc86c
Márton Kardos <power.up1163@gmail.com>
2024-05-02 08:25:29 +0200

    Added main function to assignment

--- run_analysis.py
+++ run_analysis.py
@@ -32,26 +32,31 @@ def unique_named_entities(doc: Doc) -> dict:
     }
 
 
-subdirs = glob("data/USEcorpus/*")
-subdirs = map(Path, subdirs)
-subdirs = [dir for dir in subdirs if dir.is_dir()]
+def main():
+    subdirs = glob("data/USEcorpus/*")
+    subdirs = map(Path, subdirs)
+    subdirs = [dir for dir in subdirs if dir.is_dir()]
 
-out_dir = Path("output/")
-out_dir.mkdir(exist_ok=True)
+    out_dir = Path("output/")
+    out_dir.mkdir(exist_ok=True)
 
-nlp = spacy.load("en_core_web_sm")
-for subdir in tqdm(subdirs, desc="Going through subdirectories."):
-    dir_name = subdir.stem
-    records = []
-    for file in subdir.glob("*.txt"):
-        with open(file, encoding="ISO-8859-1") as in_file:
-            text = in_file.read()
-        doc = nlp(text)
-        entry = {
-            "Filename": file.name,
-            **postag_freqs(doc),
-            **unique_named_entities(doc),
-        }
-        records.append(entry)
-    dir_table = pd.DataFrame.from_records(records)
-    dir_table.to_csv(out_dir.joinpath(f"{dir_name}.csv"))
+    nlp = spacy.load("en_core_web_sm")
+    for subdir in tqdm(subdirs, desc="Going through subdirectories."):
+        dir_name = subdir.stem
+        records = []
+        for file in subdir.glob("*.txt"):
+            with open(file, encoding="ISO-8859-1") as in_file:
+                text = in_file.read()
+            doc = nlp(text)
+            entry = {
+                "Filename": file.name,
+                **postag_freqs(doc),
+                **unique_named_entities(doc),
+            }
+            records.append(entry)
+        dir_table = pd.DataFrame.from_records(records)
+        dir_table.to_csv(out_dir.joinpath(f"{dir_name}.csv"))
+
+
+if __name__ == "__main__":
+    main()
